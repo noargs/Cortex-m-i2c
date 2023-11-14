95e43921ed51d7992720b22ba9c437781bd9edeb
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-11-14 04:15:41 +0000

    SYSTICK timer incorporated, tick every sec

--- 06_rtc/Src/main.c
+++ 06_rtc/Src/main.c
@@ -3,10 +3,21 @@
 #include "ds1307.h"
 #include "debug_uart.h"
 
+#define SYSTICK_TIM_CLK               16000000UL
+
+#define CSR_ENABLE                    (1 << 0U)    // SYST_CSR [GUM page: 249], enable the counter
+#define CSR_TICKINT                   (1 << 1U)    // Enable SysTick exception request
+#define CSR_CLKSRC                    (1 << 2U)    // ...,  0=external clock, 1=processor clock
+
 char* time_to_string(rtc_time_t *rtc_time);
 void number_to_string(uint8_t number, char* buf);
 char* date_to_string(rtc_date_t *current_date);
 char* get_day_of_week(uint8_t day);
+void output_date_time(void);
+void systick_init(uint32_t tick_hz);
+
+rtc_date_t current_date;
+rtc_time_t current_time;
 
 int main(void)
 {
@@ -15,31 +26,40 @@ int main(void)
 
   if (ds1307_init())
   {
-	printf("DS1307 chip failed to boot... cpu halt ch=1\n\r");
+	printf("\rDS1307 initialisation failed, cpu halt ch=1\n\r");
 	while (1);
   }
 
-  printf(" \n");
-  printf("         ............              \n");
-  printf("DS1307 chip intialised successfully\n");
-  printf("          ............             \n\r");
+  systick_init(1);
 
-  rtc_date_t current_date;
-  rtc_time_t current_time;
+  printf("\rDS1307 intialised successfully\n");
 
   current_date.day       = TUESDAY;
   current_date.date      = 14;
   current_date.month     = 11;
   current_date.year      = 23;
 
-  current_time.seconds   = 36;
-  current_time.hours     = 2;
-  current_time.minutes   = 51;
+  current_time.seconds   = 10;
+  current_time.hours     = 4;
+  current_time.minutes   = 14;
   current_time.time_fmt  = TIME_FORMAT_12HRS_AM;
 
   ds1307_set_current_date(&current_date);
   ds1307_set_current_time(&current_time);
 
+  output_date_time();
+
+  while (1);
+
+}
+
+void SysTick_Handler (void)
+{
+  output_date_time();
+}
+
+void output_date_time(void)
+{
   ds1307_get_current_date(&current_date);
   ds1307_get_current_time(&current_time);
 
@@ -57,10 +77,23 @@ int main(void)
   }
 
   printf("%s %s \n\r", date_to_string(&current_date), get_day_of_week(current_date.day));
+}
 
+void systick_init(uint32_t tick_hz)
+{
+  // Systick Register names in header file are different
+  // SysTick->LOAD corresponse to `SysTick Reload Value Register` [SYST_RVR]
+  // SysTick->VAL    ==>   `SysTick Current Value Register` [SYST_CVR]
+  // SysTick->CTRL   ==>   `SysTick Control and Status Register` [SYST_CSR]
 
-  while (1);
+  // 1. Reload with number of clocks per milliseconds [SYST_RVR] [GUM page: 249]
+  SysTick->LOAD = (SYSTICK_TIM_CLK/tick_hz) - 1; // for 16000 clock pulses, set RELOAD 15999 (i.e. N-1)
 
+  // 2. Clear `Systick current value register` [SYST_CVR] [GUM page: 249]
+  SysTick->VAL = 0;
+
+  // 3. Choose clock source, enable SysTick exception request, and enable the counter
+  SysTick->CTRL = CSR_CLKSRC | CSR_TICKINT | CSR_ENABLE;
 }
 
 // hh:mm:ss
